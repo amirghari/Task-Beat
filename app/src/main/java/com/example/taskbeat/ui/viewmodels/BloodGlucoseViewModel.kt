1cb4704383f8edc187623fa9649279958a5e253b
wondomen <wondomen2@gmail.com>
2024-11-12 19:20:25 +0200

    Blood pressure and Blood Glucose takes input from user

--- app/src/main/java/com/example/taskbeat/ui/viewmodels/BloodGlucoseViewModel.kt
+++ app/src/main/java/com/example/taskbeat/ui/viewmodels/BloodGlucoseViewModel.kt
@@ -2,9 +2,18 @@ package com.example.taskbeat.ui.viewmodels
 
 import androidx.lifecycle.ViewModel
 import com.example.taskbeat.data.DataRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 
 class BloodGlucoseViewModel(
     private val dataRepo: DataRepository,
 ) : ViewModel() {
+    private val _lastRecorded = MutableStateFlow<String?>(null)
+    val lastRecorded: StateFlow<String?> = _lastRecorded
+
+    fun saveGlucoseLevel(glucoseLevel: String) {
+        // Here you would save the glucose level, e.g., to a local database or repository
+        _lastRecorded.value = glucoseLevel
+    }
 
 }
